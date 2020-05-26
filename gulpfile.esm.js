47b57693b6aa8fc3fdc7bc416d9de5707696bb58
Paulo Henrique <pauloh1288@gmail.com>
2020-05-25 21:51:24 -0300

    chore: renames gulpfile.js -> gulpfile.esm.js to use ESModules

---
+++ gulpfile.esm.js
@@ -0,0 +1,35 @@
+import { src, dest, watch } from 'gulp';
+import rename from 'gulp-rename';
+import concat from 'gulp-concat';
+import postcss from 'gulp-postcss';
+import sass from 'gulp-sass';
+import cleanCSS from 'gulp-clean-css';
+import autoprefixer from 'autoprefixer';
+
+function buildSass() {
+  return src('./css/scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./css'));
+}
+
+function concatCss() {
+  return src(['./css/reset.css', './css/main.css'])
+    .pipe(concat('style.css'))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest('./css/dist/'));
+}
+
+export function buildCss() {
+  return src('./css/dist/style.css')
+    .pipe(cleanCSS())
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(dest('./css/dist/'));
+}
+
+export function watchSass() {
+  watch('./css/scss/**/*.scss', function watch(cb) {
+    buildSass();
+    concatCss();
+    cb();
+  });
+}
